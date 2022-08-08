899fd80bb41c075a4396a85a5d7c1e40168d462a
yash <ys8270292@gmail.com>
2022-08-08 12:33:24 +0530

    Initial Commit

---
+++ FileParsingJSON/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileParsingJSON
+//
+//  Created by Sam-IT on 17/05/1944 Saka.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
