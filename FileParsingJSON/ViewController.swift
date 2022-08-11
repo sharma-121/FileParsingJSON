848eb8a3fb8ea3ee2d506b0374ecc07d91a3f7d3
yash <ys8270292@gmail.com>
2022-08-11 18:22:38 +0530

    first commit mesage

--- FileParsingJSON/ViewController.swift
+++ FileParsingJSON/ViewController.swift
@@ -11,9 +11,46 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        parseJSON()
     }
-
-
+    
+    private func parseJSON() {
+        guard let path = Bundle.main.path(forResource: "data", ofType: "json") else {
+            return
+    }
+    let url = URL(fileURLWithPath: path)
+        
+        var result: Result?
+        do {
+            let jsonData = try Data(contentsOf: url)
+            result = try JSONDecoder().decode(Result.self, from: jsonData)
+    
+            if let result = result {
+                print(result)
+            }
+            else {
+                print("Failed to parse")
+            }
+            return
+        }
+        catch {
+            print("Error: \(error)")
+        }
+    }
+ 
+    
 }
 
+struct Result: Codable {
+    let data: [ResultItem]
+}
+
+
+struct ResultItem: Codable {
+    let id: String
+    let address: String
+    let name: String
+    let status: String
+}
+        
+      
